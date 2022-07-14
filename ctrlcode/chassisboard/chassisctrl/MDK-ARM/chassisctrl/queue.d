80bcac4638024b1f230a241c23281fd0a2293b60
LPGUAIA <2158494709@qq.com>
2022-07-14 15:59:52 +0800

    存一下换个机器

---
+++ ctrlcode/chassisboard/chassisctrl/MDK-ARM/chassisctrl/queue.d
@@ -0,0 +1,15 @@
+chassisctrl\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/queue.c
+chassisctrl\queue.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+chassisctrl\queue.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+chassisctrl\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h
+chassisctrl\queue.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+chassisctrl\queue.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+chassisctrl\queue.o: ../Inc/FreeRTOSConfig.h
+chassisctrl\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
+chassisctrl\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h
+chassisctrl\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h
+chassisctrl\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM4F/portmacro.h
+chassisctrl\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h
+chassisctrl\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h
+chassisctrl\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h
+chassisctrl\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/queue.h
