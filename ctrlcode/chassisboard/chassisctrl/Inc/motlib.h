115cfc4c2296854d56307e814d9a763598b60ebd
LPGUAIA <2158494709@qq.com>
2022-07-20 20:37:19 +0800

    终于在vscode上能动了。。

--- ctrlcode/chassisboard/chassisctrl/Inc/motlib.h
+++ ctrlcode/chassisboard/chassisctrl/Inc/motlib.h
@@ -8,28 +8,28 @@
 // A
 
 // 3508 parameter
-#define m3508                 0x00
-#define maxspeed_3508         500.f
-#define maxcurrent_3508       20.f
+#define m3508 0x00
+#define maxspeed_3508 500.f
+#define maxcurrent_3508 20.f
 #define current_maxvalue_3508 0x4fff
-#define currenttotorque_2006  1.f / 3.f
-#define gearratio_3508        3591.f / 187.f
+#define currenttotorque_2006 1.f / 3.f
+#define gearratio_3508 3591.f / 187.f
 
 // 2006 parameter
-#define m2006                 0x01
-#define maxspeed_2006         500.f
-#define maxcurrent_2006       10.f
+#define m2006 0x01
+#define maxspeed_2006 500.f
+#define maxcurrent_2006 10.f
 #define current_maxvalue_2006 10000
-#define currenttotorque_2006  1.f / 3.f
-#define gearratio_2006        36.f
+#define currenttotorque_2006 1.f / 3.f
+#define gearratio_2006 36.f
 
 // 6020 parameter
-#define gm6020                0x02
-#define maxspeed_6020         300.f
-#define maxcurrent_6020       2.f
+#define gm6020 0x02
+#define maxspeed_6020 300.f
+#define maxcurrent_6020 2.f
 #define current_maxvalue_6020 30000
-#define currenttotorque_6020  1.f / 2.f
-#define gearratio_6020        1.f
+#define currenttotorque_6020 1.f / 2.f
+#define gearratio_6020 1.f
 
 typedef struct _Motor_Info
 {
