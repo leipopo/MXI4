98e9b0ee1559ec8ba8fa63e8923437db57652464
LPGUAIA <2158494709@qq.com>
2023-04-11 09:10:07 +0800

    存一下滚回

--- ctrlcode/gimbalboard/gimbctrl/src/tasks/init.c
+++ ctrlcode/gimbalboard/gimbctrl/src/tasks/init.c
@@ -8,12 +8,17 @@ void chasmotinit()
 void devinit()
 {
     can_filter_init();
+    HAL_Delay(50);
     HAL_TIM_PWM_Start(&htim8, TIM_CHANNEL_3);
+    HAL_Delay(50);
     HAL_TIM_PWM_Start(&htim10, TIM_CHANNEL_1);
+    HAL_Delay(50);
 }
 
 void userinit()
 {
     chasmotinit();
+    HAL_Delay(50);
     devinit();
+    HAL_Delay(50);
 }
