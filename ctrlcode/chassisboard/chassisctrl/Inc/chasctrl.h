773cc4f383a096672b02d85e0bd8e6e33c75cfde
LPGUAIA <2158494709@qq.com>
2023-05-21 21:13:45 +0800

    修复了高速陀螺前后左右乱动的问题

--- ctrlcode/chassisboard/chassisctrl/Inc/chasctrl.h
+++ ctrlcode/chassisboard/chassisctrl/Inc/chasctrl.h
@@ -1,7 +1,7 @@
 #ifndef CCTRL_H
 #define CCTRL_H
 
-#define movespeed 500.f
+#define movespeed 400.f
 #define spinningspeed 200.f
 
 
--- ctrlcode/chassisboard/chassisctrl/Src/tasks/chasctrl.c
+++ ctrlcode/chassisboard/chassisctrl/Src/tasks/chasctrl.c
@@ -17,12 +17,51 @@ PID_regulator whespid[4];
 void xyzspeed2wheelspeed(MotorInfo mi[4])
 {
     float xytempspeed[2] = {0.f, 0.f};
-    xytempspeed[0] = robinfo.tar.xspeed * cos(robinfo.cur.zrelangle/ 360.f * 2 * 3.1415f) + robinfo.tar.yspeed * sin(robinfo.cur.zrelangle/ 360.f * 2 * 3.1415f);
-    xytempspeed[1] = -robinfo.tar.xspeed * sin(robinfo.cur.zrelangle/ 360.f * 2 * 3.1415f) + robinfo.tar.yspeed * cos(robinfo.cur.zrelangle/ 360.f * 2 * 3.1415f);
-    mi[0].tarmotorinfo.speed = -xytempspeed[0] - xytempspeed[1] + robinfo.tar.zspeed;
-    mi[1].tarmotorinfo.speed = -xytempspeed[0] + xytempspeed[1] + robinfo.tar.zspeed;
-    mi[2].tarmotorinfo.speed = xytempspeed[0] + xytempspeed[1] + robinfo.tar.zspeed;
-    mi[3].tarmotorinfo.speed = xytempspeed[0] - xytempspeed[1] + robinfo.tar.zspeed;
+    float sumspeed[4] = {0.f, 0.f, 0.f, 0.f};
+    xytempspeed[0] = robinfo.tar.xspeed * cos(robinfo.cur.zrelangle / 360.f * 2 * 3.1415f) + robinfo.tar.yspeed * sin(robinfo.cur.zrelangle / 360.f * 2 * 3.1415f);
+    xytempspeed[1] = -robinfo.tar.xspeed * sin(robinfo.cur.zrelangle / 360.f * 2 * 3.1415f) + robinfo.tar.yspeed * cos(robinfo.cur.zrelangle / 360.f * 2 * 3.1415f);
+
+    sumspeed[0] = -xytempspeed[0]  -xytempspeed[1] + robinfo.tar.zspeed;
+    sumspeed[1] = -xytempspeed[0] + xytempspeed[1] + robinfo.tar.zspeed;
+    sumspeed[2] = xytempspeed[0] + xytempspeed[1] + robinfo.tar.zspeed;
+    sumspeed[3] = xytempspeed[0] - xytempspeed[1] + robinfo.tar.zspeed;
+
+    if (fabsf(sumspeed[0]) > 400.f)
+    {
+        mi[0].tarmotorinfo.speed = -xytempspeed[0] / fabs(sumspeed[0]) * 400.f - xytempspeed[1] / fabs(sumspeed[0]) * 400.f + robinfo.tar.zspeed / fabs(sumspeed[0]) * 400.f;
+    }
+    else
+    {
+        mi[0].tarmotorinfo.speed=sumspeed[0];
+    }
+    if (fabsf(sumspeed[1]) > 400.f)
+    {
+        mi[1].tarmotorinfo.speed = -xytempspeed[0] / fabs(sumspeed[1]) * 400.f + xytempspeed[1] / fabs(sumspeed[1]) * 400.f + robinfo.tar.zspeed / fabs(sumspeed[1]) * 400.f;
+    }
+    else
+    {
+        mi[1].tarmotorinfo.speed=sumspeed[1];
+    }
+    if (fabsf(sumspeed[2]) > 400.f)
+    {
+        mi[2].tarmotorinfo.speed = xytempspeed[0] / fabs(sumspeed[2]) * 400.f + xytempspeed[1] / fabs(sumspeed[2]) * 400.f + robinfo.tar.zspeed / fabs(sumspeed[2]) * 400.f;
+    }
+    else
+    {
+        mi[2].tarmotorinfo.speed=sumspeed[2];
+    }
+    if (fabsf(sumspeed[3]) > 400.f)
+    {
+        mi[3].tarmotorinfo.speed = xytempspeed[0] / fabs(sumspeed[3]) * 400.f - xytempspeed[1] / fabs(sumspeed[3]) * 400.f + robinfo.tar.zspeed / fabs(sumspeed[3]) * 400.f;
+    }
+    else
+    {
+        mi[3].tarmotorinfo.speed=sumspeed[3];
+    }
+
+    if()
+
+
 }
 
 void init_whemot_pid(PID_regulator wspid[4])
@@ -35,7 +74,7 @@ void init_whemot_pid(PID_regulator wspid[4])
         wspid[i].componentKpMax = 10000;
         wspid[i].componentKiMax = 3000;
         wspid[i].componentKdMax = 5000;
-        wspid[i].outputMax = 6000;
+        wspid[i].outputMax = 15000;
     }
 }
 
@@ -76,7 +115,7 @@ void chasctrl()
     {
         xyzspeed2wheelspeed(whe);
         calc_whemot_spid(whespid);
-        powerctrl(4,whespid,calcmaxsumcurrentvalue(&robinfo, &capsinfo));
+        powerctrl(4, whespid, calcmaxsumcurrentvalue(&robinfo, &capsinfo));
         pack_whemot_ctrlmes(can1_mes20x200);
         if (robinfo.comd.moton)
         {
